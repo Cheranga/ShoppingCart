1a215dd653cd73505a911723955ea12f5bd9e8de
Cheranga Hatangala <cheranga@gmail.com>
2015-02-18 23:08:16 +1100

    commited

--- ShoppingCart.Web.API_bkup/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ShoppingCart.Web.API.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- ShoppingCart.Web.API_bkup/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ShoppingCart.Web.API.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
