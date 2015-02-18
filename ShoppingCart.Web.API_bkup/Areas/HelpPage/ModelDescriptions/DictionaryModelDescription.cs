7e392175d0ec1f1a8a5cad63a0793039c7b1d470
Cheranga Hatangala <cheranga@gmail.com>
2015-02-19 08:52:36 +1100

    commited

--- ShoppingCart.Web.API_bkup/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ShoppingCart.Web.API.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- ShoppingCart.Web.API_bkup/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace ShoppingCart.Web.API.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
