4e583570c5407994ba15ab4765e1ce12c98af182
Cheranga Hatangala <cheranga@gmail.com>
2015-02-26 00:32:45 +1100

    commited

---
+++ ShoppingCart.DTO/DTOs/SalesOrderDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingCart.DTO.DTOs
+{
+    public class SalesOrderDTO : IDto
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string PONumber { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
