556b08762e7b6dc5c3e9620b321f23fb8e321237
Cheranga Hatangala <cheranga@gmail.com>
2015-03-08 22:44:43 +1100

    Including "require.js" to manage scripts in the application.

---
+++ ShoppingCart.Web/Scripts/App/Views/Home/index.js
@@ -0,0 +1,6 @@
+﻿define(['jquery', 'knockout', 'komapping', 'bootstrap'], function ($, ko, kom, bs) {
+
+    if ($ && ko && kom && bs) {
+        console.log('Hooray...everything is loaded and ready to go!');
+    }
+});
---
+++ ShoppingCart.Web/Scripts/dependencies.js
@@ -0,0 +1,18 @@
+﻿require.config({
+    baseUrl: "/Scripts/",
+    paths: {
+        app: "App",
+        jquery: "/Scripts/Lib/jquery-1.10.2.min",
+        bootstrap: '/Scripts/Lib/bootstrap.min',
+        knockout: '/Scripts/Lib/knockout-3.2.0',
+        komapping: '/Scripts/Lib/knockout.mapping-latest'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        },
+        komapping: {
+            deps: ['knockout']
+        }
+    }
+});
